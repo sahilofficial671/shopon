e8b1f83ca81e798344128a413e2e15244b5bfd83
Sahil Bhatia <sahilofficial671@gmail.com>
2021-05-11 17:32:05 +0530

    Front Header moved to Front Index

--- src/app/modules/front/front-common/front-header/front-header.component.ts
+++ src/app/modules/front/front-common/front-header/front-header.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { AuthService } from 'src/app/core/services/auth.service';
-import { InstanceService } from 'src/app/core/services/instance.service';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-front-header',
@@ -10,58 +6,9 @@ import { InstanceService } from 'src/app/core/services/instance.service';
   styleUrls: ['./front-header.component.css']
 })
 export class FrontHeaderComponent implements OnInit {
-  // @ViewChild('sidenav') sidenav: MatSidenav;
-  isExpanded = true;
-  showSubmenu: boolean = false;
-  isShowing = false;
-  showSubSubMenu: boolean = false;
-
-  mouseenter() {
-    if (!this.isExpanded) {
-      this.isShowing = true;
-    }
-  }
-
-  mouseleave() {
-    if (!this.isExpanded) {
-      this.isShowing = false;
-    }
-  }
-
-  links = [
-    {"url":"/", "name": "Home"},
-  ];
-
-  adminLinks = [
-    {"url":"admin/dashboard", "name": "Dashboard"},
-  ];
-
-  constructor(private router: Router,
-  private authService: AuthService,
-  private instanceService: InstanceService,
-  private toastr: ToastrService,
+  constructor(
   ) { }
 
   ngOnInit(): void {
   }
-
-  isAdmin():boolean{
-    if(this.router.url.startsWith("/admin")){
-      return true;
-    }
-    return false;
-  }
-
-  adminLogout():void{
-    this.authService.adminLogout();
-    this.router.navigateByUrl("/admin/login");
-    this.toastr.success("Successfuly logged out.")
-  }
-
-  adminLoggedIn():boolean{
-    return this.authService.hasAuth()
-    && this.instanceService.getAuthUser()
-    && this.instanceService.getAuthUser().hasRoleAdmin();
-  }
-
 }
