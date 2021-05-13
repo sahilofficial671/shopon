19f631a28e2a6afc26c780421a328f3334e7fda2
Sahil Bhatia <sahilofficial671@gmail.com>
2021-05-13 20:24:19 +0530

    Routing strategy changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminAuthGuard } from './core/guards/admin-auth.guard';
+import { AdminGuard } from './core/guards/admin.guard';
 import { GuestAdminGuard } from './core/guards/guest-admin.guard';
 import { AdminLoginComponent } from './modules/admin/admin-auth/admin-login/admin-login.component';
 import { AdminDashboardComponent } from './modules/admin/admin-dashboard/admin-dashboard.component';
+import { AdminProductListComponent } from './modules/admin/admin-product/admin-product-list/admin-product-list.component';
+import { AdminProductUpdateComponent } from './modules/admin/admin-product/admin-product-update/admin-product-update.component';
 import { TestComponent } from './modules/admin/test/test.component';
 import { FrontIndexComponent } from './modules/front/front-common/front-index/front-index.component';
 import { Error404Component } from './shared/errors/error404/error404.component';
@@ -23,8 +25,12 @@ const routes: Routes = [
 
   // Admin Protected Routes
   {
-    path: 'admin', canActivate: [AdminAuthGuard], children: [
+    path: 'admin', canActivate: [AdminGuard], children: [
       { path: "dashboard", component:  AdminDashboardComponent},
+      { path: "products", children: [
+        { path: "list", component:  AdminProductListComponent},
+        { path: ":id", component:  AdminProductUpdateComponent},
+      ]},
     ]
   },
 
@@ -33,6 +39,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
