741a0e12644c27e015adc4257b916bda36b6b23c
sahil <sahilofficial671@gmail.com>
2022-11-27 00:49:22 +0530

    Tweak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  server_url: "https://ecommerce-api-node.vercel.app/",
+  server_url: "https://ecommerce-api-node.vercel.app",
   imageKitUrl: 'https://ik.imagekit.io/webiggle/products/',
   noImagePath: '/assets/images/common/no-image.png',
 };
