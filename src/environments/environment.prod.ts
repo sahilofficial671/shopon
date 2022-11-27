04971bf876ad93da538f4f3e6d56d1d6fc437142
sahil <sahilofficial671@gmail.com>
2022-11-27 15:41:34 +0530

    Changed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  server_url: "https://ecommerce-api-node.vercel.app/api/v1",
+  server_url: "https://ecommerce.webiggle.com/api/v1",
   imageKitUrl: 'https://ik.imagekit.io/webiggle/products/',
   noImagePath: '/assets/images/common/no-image.png',
 };
